df88543db00933884f13841c6615697f49e4dc25
alex_asemota <asemota.alexander@gmail.com>
2020-03-24 01:45:15 +0000

    Added light comments to repack_images.py

--- repack_images.py
+++ repack_images.py
@@ -2,15 +2,15 @@ import os
 import shutil
 from tqdm import tqdm
 
+'''
+The images from the ImageNet website are not sorted into folders by class
+This script does that for the 2010 validation data
+'''
+
 filenames = os.listdir('val')
 filenames.sort()
 val_labels = open('/home/asemota_alexander/data/ILSVRC2010_val_ground_truth.txt', 'r').readlines()
 
-'''
-truth_dict = dict()
-for i in range(1000):
-    truth_dict[i] = []
-'''
 print(min(val_labels))
 for i in range(1000):
     if not os.path.exists(os.path.join('/home/asemota_alexander/data/val_2010', str(i))):
@@ -20,3 +20,8 @@ for i in tqdm(range(len(filenames))):
     label = int(val_labels[i])
     shutil.move(os.path.join('/home/asemota_alexander/project/val',filenames[i]), 
             os.path.join('/home/asemota_alexander/data/val_2010',str(label)))
+
+'''
+There seem to only be classes 1-999, instead of 0-999
+I'm not sure if this means I'm missing something or not
+'''
